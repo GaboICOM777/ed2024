ee73827f97612046b8f39e21093716f32b0b9027
GaboICOM777 <159519453+GaboICOM777@users.noreply.github.com>
2024-03-07 23:44:55 -0600

    Add files via upload
    Tarea

---
+++ Tarea_5_Diccionario/app.py
@@ -0,0 +1,23 @@
+# app.py
+
+from flask import Flask, request, render_template, send_file
+from diccionario_productos import productos
+from generar_recibo import generar_recibo
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route('/')
+def index():
+    return render_template('index.html', productos=productos)
+
+@app.route('/generar_recibo', methods=['POST'])
+def generar_recibo_endpoint():
+    productos_seleccionados = {}
+    for producto in request.form.getlist('producto'):
+        productos_seleccionados[producto] = productos[producto]
+
+    pdf_filename = generar_recibo(productos_seleccionados)
+    return send_file(pdf_filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
