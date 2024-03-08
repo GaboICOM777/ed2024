d743dd67ae8ef3794fb5c7442af9dba2d5d58f7e
GaboICOM777 <159519453+GaboICOM777@users.noreply.github.com>
2024-03-07 23:22:47 -0600

    DiccionarioTarea

---
+++ Tarea_5_Diccionario/diccionario_productos.py
@@ -0,0 +1,14 @@
+# diccionario_productos.py
+
+productos = {
+    "Producto 1": 10.00,
+    "Producto 2": 15.00,
+    "Producto 3": 20.00,
+    "Producto 4": 25.00,
+    "Producto 5": 30.00,
+    "Producto 6": 35.00,
+    "Producto 7": 40.00,
+    "Producto 8": 45.00,
+    "Producto 9": 50.00,
+    "Producto 10": 55.00
+}
---
+++ Tarea_5_Diccionario/generar_recibo.py
@@ -0,0 +1,36 @@
+import os
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from diccionario_productos import productos
+
+def generar_recibo(productos_seleccionados):
+    # Ruta del directorio actual del script
+    directorio_actual = os.path.dirname(__file__)
+    
+    # Ruta de la carpeta donde se guardará el archivo PDF
+    carpeta_destino = os.path.join(directorio_actual, "Recibos_compra")
+
+    # Comprueba si la carpeta de destino existe, y si no, créala
+    if not os.path.exists(carpeta_destino):
+        os.makedirs(carpeta_destino)
+
+    # Nombre del archivo PDF
+    pdf_filename = os.path.join(carpeta_destino, "recibo.pdf")
+
+    c = canvas.Canvas(pdf_filename, pagesize=letter)
+    y = 750
+    total = 0
+
+    c.drawString(100, y, "Recibo de Compra:")
+    y -= 20
+
+    for producto, precio in productos_seleccionados.items():
+        c.drawString(100, y, f"{producto}: ${precio}")
+        total += precio
+        y -= 20
+
+    c.drawString(100, y, f"Total: ${total}")
+
+    c.save()
+
+    return pdf_filename
